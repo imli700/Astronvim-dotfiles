9a99ec2b833db359d7f7c08ff76e777954609c2a
imli700 <imlijangba@gmail.com>
2025-10-09 13:36:26 +0530

    A lot of stuff

---
+++ lua/community.lua
@@ -0,0 +1,19 @@
+if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+
+-- AstroCommunity: import any community modules here
+-- We import this file in `lazy_setup.lua` before the `plugins/` folder.
+-- This guarantees that the specs are processed before any user plugins.
+
+---@type LazySpec
+return {
+  "AstroNvim/astrocommunity",
+  { import = "astrocommunity.pack.lua" },
+  -- import/override with your plugins folder
+  { import = "astrocommunity.pack.typescript-all-in-one"}
+  { import = "astrocommunity.pack.json"}
+  { import = "astrocommunity.pack.java"}
+  { import = "astrocommunity.pack.cpp"}
+  { import = "astrocommunity.pack.python"}
+  { import = "astrocommunity.pack.markdown"}
+  { import = "astrocommunity.pack.html-css"}
+}
